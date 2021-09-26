1b7a3300a69968301aa1e332c2ae1a43c3de9ceb
Julio Filgueiras <julio_filgueiras@live.com>
2021-09-26 01:15:28 -0300

    feat(urna eletrônica): exibição do candidato

--- urna_eletronica/script.js
+++ urna_eletronica/script.js
@@ -18,8 +18,9 @@ function comecarEtapa() {
 
         if (i === 0) {
             numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
         }
-        numeroHtml += '<div class="numero"></div>';
     }
 
     seuVotoPara.style.display = 'none';
@@ -31,7 +32,30 @@ function comecarEtapa() {
 }
 
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
 
+    let candidato = etapa.candidatos.filter((item)=> {
+        if (item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if (candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+
+        for (let i in candidato.fotos) {
+            fotosHtml = `<div class="d-1-image"> <img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda} </div>`;
+        }
+
+        imagensCandidatos.innerHTML = fotosHtml;
+    }
 }
 
 function clicou(n) {
