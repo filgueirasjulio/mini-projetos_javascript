678fcd8dfe8fc787e2809ed9f2c12d25d7df3ce7
Julio Filgueiras <julio_filgueiras@live.com>
2021-09-25 23:48:48 -0300

    feat(urna eletrônica): controle das área e interfaca da 1ª tela

---
+++ urna_eletronica/script.js
@@ -0,0 +1,67 @@
+//variaveis de controle de interface
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let imagensCandidatos = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+//variaveis de controle de ambiente
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = '';
+
+    for(let i = 0; i < etapa.numeros; i++) {
+
+        if (i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        }
+        numeroHtml += '<div class="numero"></div>';
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    imagensCandidatos.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+
+}
+
+function clicou(n) {
+
+   let elementoNumero = document.querySelector('.numero.pisca');
+
+   if (elementoNumero !== null) {
+       elementoNumero.innerText = n;
+       numero = `${numero}${n}`;
+
+       elementoNumero.classList.remove('pisca');
+
+       if (elementoNumero.nextElementSibling != null) {
+            elementoNumero.nextElementSibling.classList.add('pisca');
+       } else {
+            atualizaInterface();
+       }   
+   } 
+}
+
+function branco() {
+    alert("Clicou em BRANCO!");
+}
+
+function corrige() {
+    alert("Clicou em CORRIGE!");
+}
+
+function confirma() {
+    alert("Clicou em CONFIRMA!");
+}
+
+comecarEtapa()
