0563d97f06a4ca82188a8091672f8fe068c16086
filgueiras.julio <julio_filgueiras@live.com>
2023-04-19 00:57:30 -0300

    refactor(projeto pizzaria): refatoração de código
    Redução dos arquivos js

--- pizzaria/js/variaveisGerais.js
+++
@@ -1,6 +0,0 @@
-const d = (element) => document.querySelector(element);
-const da = (element) => document.querySelectorAll(element);
-const cart = [];
-
-let key;
-let size = parseInt(d('.pizzaInfo--size.selected').getAttribute('data-key'));
